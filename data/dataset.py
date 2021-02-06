00b697d598e00f69bbef6a34786e031127abd852
2anandjha <70034867+2anandjha@users.noreply.github.com>
2021-02-06 18:48:47 +0530

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,10 +1,29 @@
 from torchvision import datasets, transforms
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+
 
 
 def cifar10_dataset(location='../data', train=True, download=True, transform=None):
     return datasets.CIFAR10(location, train=train, download=download, transform=transform)
 
+''' using pytorch albumenations'''
 
+train_transform = A.Compose(
+    [
+        A.SmallestMaxSize(max_size=160),
+        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+        A.RandomCrop(height=128, width=128),
+        A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+        A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ToTensorV2(),
+    ]
+)
+
+'''
 def transformations(augmentation=False, rotation=3.0):
     transforms_list = [
         transforms.ToTensor(),
@@ -15,4 +34,4 @@ def transformations(augmentation=False, rotation=3.0):
                               transforms.RandomRotation((-rotation, rotation), fill=(1,))
                           ] + transforms_list
 
-    return transforms.Compose(transforms_list)
+    return transforms.Compose(transforms_list)'''
